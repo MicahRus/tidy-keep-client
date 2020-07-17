64970ba4515d0152f5335b3b407ffa258623ca23
Micah <Rus.Micah@gmail.com>
2020-07-17 14:51:19 +1000

    Added logic for caluclating cost on front page based off of user input

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,24 +1,51 @@
 import React, { Component } from "react";
 
 class Landing extends Component {
+  state = { bedrooms: 1, bathrooms: 1, choice: "Standard" };
+  // A method to calculate the cost of the currently selected cleaning items
+  calculateCost = () => {
+    let bedroomCost = this.state?.bedrooms * 35;
+    let bathroomCost = this.state?.bathrooms * 35;
+    let costMultiplier = 1
+    // Potential logic for editing price based on pack
+    if (this.state.choice === "Deluxe") {
+       costMultiplier = 2;
+    } else if (this.state.choice === "Deep clean") {
+       costMultiplier = 3;
+    } else if (this.state.choice === "Moving in/out") {
+       costMultiplier = 4;
+    }
+
+    let totalCost = ((bedroomCost + bathroomCost) * costMultiplier)
+
+    return <div> {totalCost} </div>;
+  };
+
   // A method that will return the value of the calculated cost.
   showValue = () => {
     return (
-      <div> Test </div>
-    )
-  }
-  // This will handle refreshing the dom when the select buttons are changed
+      <div>
+        test
+        {this.calculateCost()}
+      </div>
+    );
+  };
+  // This will handle setting the state when button choices are changed
   handleChange = (event) => {
-    console.log('test');
-    console.log(event.target.value);
-  }
+    if (event.target.value.includes("bedroom")) {
+      this.setState({ bedrooms: event.target.value[0] });
+    } else if (event.target.value.includes("bathroom")) {
+      this.setState({ bathrooms: event.target.value[0] });
+    } else {
+      this.setState({ choice: event.target.value });
+    }
+  };
 
-// A form containing the select buttons on the homepage.
+  // A form containing the select buttons on the homepage.
   form = () => {
     return (
       <form onChange={this.handleChange}>
         <select>
-
           <option value="1 bedroom">1 bedroom</option>
           <option value="2 bedroom">2 bedroom</option>
           <option value="3 bedroom">3 bedroom</option>
@@ -27,7 +54,6 @@ class Landing extends Component {
         </select>
 
         <select>
-
           <option value="1 bathroom">1 bathroom</option>
           <option value="2 bathroom">2 bathroom</option>
           <option value="3 bathroom">3 bathroom</option>
@@ -35,20 +61,20 @@ class Landing extends Component {
         </select>
 
         <select>
-
           <option value="Standard">Standard </option>
           <option value="Deluxe">Deluxe </option>
-          <option value="">Deep clean </option>
-          <option value="Deep">Moving in/out </option>
+          <option value="Deep clean">Deep clean </option>
+          <option value="Moving in/out">Moving in/out </option>
         </select>
 
-        <input type='submit' name='submit' />
+        <input type="submit" name="submit" />
 
         {this.showValue()}
       </form>
     );
   };
   render() {
+    console.log(this.state);
     return (
       <>
         <h1>On Landing</h1>
