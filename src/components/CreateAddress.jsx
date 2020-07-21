32a5f117918c00abd38a62bcde64fa2395154a64
Micah <Rus.Micah@gmail.com>
2020-07-21 23:36:16 +1000

    Added user_id to initial state, and changed logic in 'handleChange' to set the state for the 'state' properly.

--- src/components/CreateAddress.jsx
+++ src/components/CreateAddress.jsx
@@ -1,41 +1,46 @@
 import React from "react";
 
 class CreateAddress extends React.Component {
-   state = {
+  state = {
     street_address: "",
     post_code: "",
     // suburb: "",
     state: "VIC",
+    user_id: 1
   };
-  
+
   onInputChange = (event) => {
-    console.log("on input change");
+    console.log(this.state);
+    console.log(event.target.id);
+    console.log(event.target.value);
     this.setState({
       [event.target.id]: event.target.value,
     });
   };
 
-  handleChange= (event) => {
+  handleChange = (event) => {
+      console.log('here');
+      console.log(this.state);
+      console.dir(event.target.value );
     this.setState({
-      [event.target.id]: event.target.value,
+      state: event.target.value,
     });
   };
 
   onFormSubmit = async (event) => {
     event.preventDefault();
-  
 
     await fetch("http://localhost:3000/addresses", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({address: this.state}),
+      body: JSON.stringify({ address: this.state }),
     });
     this.props.history.push("/addresses");
   };
-  
-// {address: this.state.address, post_code: this.state.post_code, state: this.state.value}
+
+  // {address: this.state.address, post_code: this.state.post_code, state: this.state.value}
 
   render() {
     return (
@@ -49,29 +54,24 @@ class CreateAddress extends React.Component {
             id="street_address"
             onChange={this.onInputChange}
           />
-                    {/* <label htmlFor="name">Suburb</label>
-          <input
-            type="text"
-            name="suburb"
-            id="suburb"
-            onChange={this.onInputChange}
-          /> */}
+
           <label htmlFor="post_code">Post code</label>
           <textarea
             name="post_code"
             id="post_code"
             onChange={this.onInputChange}
           ></textarea>
+
+
           <label htmlFor="state"> State</label>
-          <select value={this.state.value} onChange={this.handleChange}>
+          <select defaultValue={this.state.value} onChange={this.handleChange}>
             <option value="VIC">VIC</option>
             <option value="TAS">Tasmania</option>
             <option value="NSW">NSW</option>
             <option value="ACT">ACT</option>
             <option value="WA">WA</option>
-            </select>
+          </select>
           <input type="submit" value="Submit" />
-    
         </form>
       </div>
     );
