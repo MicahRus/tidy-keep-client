b9ed20403802180d332ed3515a150f7177b7548b
Micah <Rus.Micah@gmail.com>
2020-07-27 14:08:49 +1000

    Not much

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React from "react";
+import { RRule } from "rrule";
+
+const rule = new RRule({
+  freq: RRule.WEEKLY, // repeat weekly, possible freq [DAILY, WEEKLY, MONTHLY, ]
+  interval: 1,
+  byweekday: [RRule.MO],
+  dtstart: new Date(2020, 6, 20, 10, 30),
+  count: 4,
+});
+
+console.log(rule.all());
+console.log(rule.all().length);
 
 class Confirm extends React.Component {
-  state = { data: this.props.location.state.data }
-
+  state = { data: this.props.location.state.data };
 
   componentDidMount() {
-    this.getServicesData()
+    this.getServicesData();
   }
 
-  // A function that will post the booking data into the database 
+  // A function that will post the booking data into the database
   postBookingData = async () => {
-    
     // Sets the data that will be parsed into the post request
     let data = {
       recurring: true,
       price: this.state.data.data.pricing.totalCost,
       datetime: this.state.data.data.startDate,
-      address_id: this.state.data.userChoice
-      
-    }
+      address_id: this.state.data.userChoice,
+    };
 
-      await fetch(`${process.env.REACT_APP_API}/bookings`, {
+    await fetch(`${process.env.REACT_APP_API}/bookings`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-      body: JSON.stringify({booking: data}),
+      body: JSON.stringify({ booking: data }),
     });
-    this.getBookingData()
-  }
+    this.getBookingData();
+  };
 
   getServicesData = async () => {
     const response = await fetch(`${process.env.REACT_APP_API}/services`);
@@ -37,135 +46,126 @@ class Confirm extends React.Component {
     this.setState({ services: data.reverse() });
   };
 
-
-
   getBookingData = async () => {
     let response = await fetch(`${process.env.REACT_APP_API}/bookings`, {
       method: "GET",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    }) 
-      let data = await response.json();
-      this.setState ({ bookings: data })
-      this.setPricing()
-  }
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    let data = await response.json();
+    this.setState({ bookings: data });
+    this.setPricing();
+  };
+
   postBookingServicesData = async (quantity, service, bookingId) => {
-    
     let data = {
       booking_id: bookingId,
       service_id: service,
-      quantity: quantity
-    }
+      quantity: quantity,
+    };
     await fetch(`${process.env.REACT_APP_API}/booking_service`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-      body: JSON.stringify({bookingservice: data}),
+      body: JSON.stringify({ bookingservice: data }),
     });
-  }
+    console.log(bookingId);
+  };
 
   setPricing = () => {
-    let bookingId = this.state.bookings.reverse()[0].id
-    let pricing = this.state.data.data.pricing
+    let bookingId = this.state.bookings.reverse()[0].id;
+    let pricing = this.state.data.data.pricing;
 
-    for( let i = 0; i < 7; i ++){
-      let quantity = 1
-      let service = i
+    for (let i = 0; i < 7; i++) {
+      let quantity = 1;
+      let service = i;
 
       // Checks for the number of bathrooms and passes it through as a variable
-      if (i === 0){
-        quantity = pricing.bathrooms
-        service = 1
+      if (i === 0) {
+        quantity = pricing.bathrooms;
+        service = 1;
       }
 
       // Same as above but for bedrooms
-      if (i === 1){
-        quantity = pricing.bedrooms
-        service = 2
+      if (i === 1) {
+        quantity = pricing.bedrooms;
+        service = 2;
       }
 
-    // Check what type of clean it is and passes that service number through as a variable
-      if (i === 2){
-      this.state.services.find((item) =>{
-        if (item.title === pricing.type.toLowerCase()) {
-          service = item.id
-        }
-      })
-      // Loops through an array of addons, comparing each of them to the services array and matching them then passing their service number through as a variable
+      // Check what type of clean it is and passes that service number through as a variable
+      if (i === 2) {
+        this.state.services.find((item) => {
+          if (item.title === pricing.type.toLowerCase()) {
+            service = item.id;
+          }
+        });
+        // Loops through an array of addons, comparing each of them to the services array and matching them then passing their service number through as a variable
         pricing.addons.map((addon) => {
-          this.state.services.find((item) =>{
-            if (addon.toLowerCase() === item.title.toLowerCase()){
-              service = item.id
+          this.state.services.find((item) => {
+            if (addon.toLowerCase() === item.title.toLowerCase()) {
+              service = item.id;
             }
-          })
-        })
+          });
+        });
+      }
+
+      this.postBookingServicesData(quantity, service, bookingId);
+      console.log('setPrice');
+    }
+  };
+
+  handleClick = (event) => {
+    for (let i = 0; i < 4; i++){
+      this.postBookingData();
+      console.log('Handleclick');
     }
 
-    this.postBookingServicesData(quantity, service, bookingId)
-      
-    }
-  }
-  
-
-  handleClick = async (event) => {
-    this.postBookingData()
-  }
-
-
-
-
-
-
+  };
 
   showData = () => {
     // Sets variables that are repeated, to help maintain code dryness
-    const location = this.state.data
-    const pricing = this.state.data.data.pricing 
+    const location = this.state.data;
+    const pricing = this.state.data.data.pricing;
     return (
       <div>
         <h3>Address</h3>
         <p>{location.selectedAddress}</p>
 
+        <h3>On</h3>
+        <p>{this.state.data.data.startDate.toString()}</p>
 
-          <h3>On</h3>
-          <p>{this.state.data.data.startDate.toString()}</p>
+        <h3>For</h3>
+        <p>Bathrooms: {pricing.bathrooms}</p>
+        <p>Bedrooms: {pricing.bedrooms}</p>
+        <p>Type: {pricing.type}</p>
+        <p>TotalCost: {pricing.totalCost}</p>
 
-          <h3>For</h3>
-          <p>Bathrooms: {pricing.bathrooms}</p>
-          <p>Bedrooms: {pricing.bedrooms}</p>
-          <p>Type: {pricing.type}</p>
-          <p>TotalCost: {pricing.totalCost}</p>
-
-          <p>Addons: {pricing.addons.map((addon) => {
-            return (
-              `${addon}, `
-            )
+        <p>
+          Addons:{" "}
+          {pricing.addons.map((addon) => {
+            return `${addon}, `;
           })}
-          </p>
+        </p>
 
-             <h1> Is this information correct?</h1>
+        <h1> Is this information correct?</h1>
 
-          <button onClick={this.handleClick}> Confirm booking</button>
-
+        <button onClick={this.handleClick}> Confirm booking</button>
       </div>
-    )
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div> Confirmation page</div>
+
+        {this.showData()}
+      </div>
+    );
   }
-
- render() {
- return (
-   <div>
-
-   <div> Confirmation page</div>
-
-    {this.showData()}
-
-   
-   </div>
- );
- };
 }
 
 export default Confirm;
