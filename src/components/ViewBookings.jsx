3693647f0adfc4d6162814c469c9a58c7578e303
Micah <Rus.Micah@gmail.com>
2020-07-29 17:04:15 +1000

    removed unused components

--- src/components/ViewBookings.jsx
+++ src/components/ViewBookings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Segment, Container, Header, Form, Button } from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 
 class ViewBookings extends React.Component {
   state = {
