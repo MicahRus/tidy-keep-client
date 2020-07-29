ca82c6d52d644646161c2c17fc5e731d852db00d
GorginZ <georgia.z.leng@gmail.com>
2020-07-29 13:48:28 +1000

    removed sign up from logged in version of navbar bc unecessary, added viewbookings to signed-in navbar so user can esily access their bookings and delete them if they want

--- src/components/ViewBookings.jsx
+++ src/components/ViewBookings.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import {
-  Segment,
-  Container,
-  Header,
-  Form,
-  Button,
-} from "semantic-ui-react";
+import { Segment, Container, Header, Form, Button } from "semantic-ui-react";
 
 class ViewBookings extends React.Component {
   state = {
-    // street_address: "",
-    // post_code: "",
-    // state: "VIC",
-    // redirect: null,
-    // data: this.props.location.state.data,
-    // addresses: [],
-    // userChoice: null,
-    // selectedAddress: "",
-    primaryColour: "CornflowerBlue",
+   
     bookings: [],
   };
 
@@ -45,10 +31,10 @@ class ViewBookings extends React.Component {
     if (window.confirm("Click OK to cancel this booking")) {
       this.deleteBooking(id);
     }
-  }
-
+  };
+//deletes specified address by id in db
   deleteBooking = async (id) => {
-    await fetch(`http://localhost:3000/bookings/${id}`, {
+    await fetch(`${process.env.REACT_APP_API}/bookings/${id}`, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -59,54 +45,51 @@ class ViewBookings extends React.Component {
 
   renderBookings = () => {
     return this.state.bookings.map((booking, index) => {
-      let bookingDate = Date(booking.datetime);
       return (
         <div key={index}>
           <div value={booking.id} className="booking">
-         <p><h4>Address</h4></p>
+            <p>
+              <h4>Address</h4>
+            </p>
             <p>
               {booking.address.street_address},{booking.address.state},{" "}
               {booking.address.post_code}
             </p>
-            <p><h4>Date</h4></p> <p>{ bookingDate}</p> 
-               <p><h4>Charge</h4></p> <p>{booking.price}</p> 
+            <p>
+              <h4>Date</h4>
+            </p>{" "}
+            <p>{booking.datetime}</p>
+            <p>
+              <h4>Charge</h4>
+            </p>{" "}
+            <p>{booking.price}</p>
           </div>
           <hr />
           <div className="delete-container">
-            <p><button class="ui negative basic button" onClick={() => this.confirmDelete(booking.id)}>
-              Delete
-            </button></p>
+            <p>
+              <button
+                class="ui negative basic button"
+                onClick={() => this.confirmDelete(booking.id)}
+              >
+                Delete
+              </button>
+            </p>
           </div>
         </div>
       );
     });
   };
 
-  // addressOnClick = (event) => {
-  //   event.preventDefault();
-  //   this.setState(
-  //     {
-  //       userChoice: event.target.value,
-  //       selectedAddress: event.target.innerText,
-  //     },
-  //     console.log(this.state)
-  //   );
-  // };
 
-  // addressStyleSelect = (position) => {
-  //   if (this.state.userChoice?.includes(position)) {
-  //     return {
-  //       backgroundColor: this.state.primaryColour,
-  //     };
-  //   }
-  // };
 
   render() {
     return (
       <>
         <div className="booking-container">
           <Segment stacked>
-            <p><h1>Current Bookings</h1></p>
+            <p>
+              <h1>Current Bookings</h1>
+            </p>
             <div>{this.renderBookings()}</div>
           </Segment>
         </div>
