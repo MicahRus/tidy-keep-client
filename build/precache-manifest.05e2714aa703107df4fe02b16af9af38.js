1c676e01c210cac1b1d50c1fbc6378f9ac79da56
Micah <Rus.Micah@gmail.com>
2020-07-30 11:58:29 +1000

    Changed logic to be able to deploy from console, and .env's changed

---
+++ build/precache-manifest.05e2714aa703107df4fe02b16af9af38.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cc08fbae97e6142ef2ade11a8f210e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d237706e259a85a684b",
+    "url": "/static/css/2.002462ae.chunk.css"
+  },
+  {
+    "revision": "dd02ab3060cd934a9fda",
+    "url": "/static/css/main.479a12b5.chunk.css"
+  },
+  {
+    "revision": "1d237706e259a85a684b",
+    "url": "/static/js/2.f29d906a.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/static/js/2.f29d906a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd02ab3060cd934a9fda",
+    "url": "/static/js/main.95054f01.chunk.js"
+  },
+  {
+    "revision": "9c8229cf8b5f22067929",
+    "url": "/static/js/runtime-main.df623eb2.js"
+  },
+  {
+    "revision": "583187ab6af9fd0f03f36a23d4a3820a",
+    "url": "/static/media/about-us-family.583187ab.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "5bb3b7ff80ce360d56eba8df73860941",
+    "url": "/static/media/cover-image.5bb3b7ff.jpg"
+  },
+  {
+    "revision": "1ec733f295750beda174f57917f702d6",
+    "url": "/static/media/dishes.1ec733f2.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "91e646f66f271a681bab46a27668e0cb",
+    "url": "/static/media/glasses.91e646f6.png"
+  },
+  {
+    "revision": "e1a032c31c6db85ed71a87fbbf1f616a",
+    "url": "/static/media/housekeeper1.e1a032c3.jpeg"
+  },
+  {
+    "revision": "2014c2e265edbca1a0361f9de5491726",
+    "url": "/static/media/housekeeper2.2014c2e2.jpeg"
+  },
+  {
+    "revision": "16747795eaab32bce72603a21aff1c33",
+    "url": "/static/media/housekeeper3.16747795.jpeg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "14d385ad636902e1f25b234d963ead9f",
+    "url": "/static/media/sponge.14d385ad.png"
+  }
+]);
